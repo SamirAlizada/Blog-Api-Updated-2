636297ecb46a54a64e53cad0397f7a6319de5467
samir203 <samir.elizade5555@gmail.com>
2022-05-29 16:20:58 +0400

    updated

---
+++ blog/api/v1/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('blogs/', views.BogListCreateApiView.as_view()),
+    path('blogs/<int:pk>', views.BlogDetailApiView.as_view()),
+
+    path('users/', views.UserListCreateApiView.as_view()),
+    path('users/<int:pk>', views.UserDetailApiView.as_view(), name='user_detail'),
+
+    path('author-point/', views.AuthorPointListCreateApiView.as_view()),
+    path('author-point/<int:pk>', views.AuthorPointDetailApiView.as_view()),
+]
